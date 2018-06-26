f551894cf40c30c212072f35ae16c7967b3322f8
Lakjeewa <lakjeewa@Populos-Mac-mini-2.local>
2018-06-26 18:36:38 +0530

    accidently worked with login branch but files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {RegisterComponent} from './register/register/register.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Wije Chat App';
+  title = 'Wije Contact App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RegisterModule} from './register/register.module';
+// components
 import { AppComponent } from './app.component';
+import {RegisterComponent} from './register/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RegisterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
