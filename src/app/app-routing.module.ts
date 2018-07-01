b85765e01e53f54defe1c59dccc37dbd133880d3
unknown <lakjeewa.2016288@iit.ac.lk>
2018-07-01 20:37:26 +0530

    declared login service modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login/login.component';
+import {LoginRouteGuard} from './login/login-route.guard';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoginRouteGuard],
+  }
 ];
 @NgModule({
   imports: [
